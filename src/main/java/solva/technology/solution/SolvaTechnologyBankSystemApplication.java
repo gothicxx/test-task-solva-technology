050822a004b6cc04d42b8e4ee28eafb1d3e31376
Александр <alexandrsinko2000@gmail.com>
2025-05-01 14:03:16 +0300

    initial commit

---
+++ src/main/java/solva/technology/solution/SolvaTechnologyBankSystemApplication.java
@@ -0,0 +1,13 @@
+package solva.technology.solution;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SolvaTechnologyBankSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SolvaTechnologyBankSystemApplication.class, args);
+	}
+
+}
